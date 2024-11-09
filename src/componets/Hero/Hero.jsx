14d76b00b0d1b6c085a68d6de83d2423a0978632
Daniyar410 <ramziya.shakirova@gmail.com>
2024-11-09 20:56:56 +0500

    Add files via upload

---
+++ src/componets/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+    <>
+    <section className="section">
+        <div className="container">
+            <div className="wrap">
+                <img src="https://i.pinimg.com/736x/12/a7/fb/12a7fbb2ba29ce90d8e08e8519490dfa.jpg" alt="" />
+                <div className="box">
+                <h2>Lorem.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br /> Corporis assumenda nobis quasi ullam dignissimos. <br /> Eius porro exercitationem assumenda fugiat quam?</p>
+                <button>Lorem.</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+    )
+}
+
+export default Hero
---
+++ src/componets/Wrap/Wrap.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import  './Wrap.scss'
+
+const Wrap = () => {
+  return (
+    <>
+    <section className="wrap2">
+            <h1>Lorem.</h1>
+        <div className="container">
+            <div className="box2">
+                
+                    <h2 className="text ">Lorem.</h2>
+                    <h2 className="text text2">Lorem.</h2>
+                    <h2 className="text">Lorem.</h2>
+                    <h2 className="text text2">Lorem.</h2>
+                    <h2 className="text">Lorem.</h2>
+                
+            </div>
+        </div>
+    </section>
+    </>
+    )
+}
+
+export default Wrap
